ba807a274743a0488451ef6d46669802b09f5847
therealsujji <sujithacrysis@gmail.com>
2019-06-05 20:49:03 +0530

    intial stage done

---
+++ Millionaire/Assets/Scripts/GameManager.cs
@@ -0,0 +1,213 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+using System.Linq;
+
+public class GameManager : MonoBehaviour
+{
+
+
+    static int noQuestions = 10;
+    Question[] questionArray = new Question[noQuestions];
+    static List<Question> unansweredQuestion;
+    Question curentQuestion;
+
+    static TextMeshProUGUI questionText;
+
+    static Button answer1Btn;
+    static Button answer2Btn;
+    static Button answer3Btn;
+    static Button answer4Btn;
+    static Button confirmBtn;
+
+    static TextMeshProUGUI answer1Text;
+    static TextMeshProUGUI answer2Text;
+    static TextMeshProUGUI answer3Text;
+    static TextMeshProUGUI answer4Text;
+
+    static GameObject confirmPanel;
+
+    static bool gameOver = false;
+
+    int timeForNextQuestion = 1;
+    int temporaryAnswer;
+
+
+    void Awake()
+    {
+
+        confirmPanel = GameObject.Find("confirmationPanel");
+        questionText = GameObject.Find("question").GetComponent<TextMeshProUGUI>();
+        answer1Btn = GameObject.Find("answer1Btn").GetComponent<Button>();
+        answer2Btn = GameObject.Find("answer2Btn").GetComponent<Button>();
+        answer3Btn = GameObject.Find("answer3Btn").GetComponent<Button>();
+        answer4Btn = GameObject.Find("answer4Btn").GetComponent<Button>();
+        confirmBtn = GameObject.Find("yes").GetComponent<Button>();
+
+        answer1Text = GameObject.Find("answer1").GetComponent<TextMeshProUGUI>();
+        answer2Text = GameObject.Find("answer2").GetComponent<TextMeshProUGUI>();
+        answer3Text = GameObject.Find("answer3").GetComponent<TextMeshProUGUI>();
+        answer4Text = GameObject.Find("answer4").GetComponent<TextMeshProUGUI>();
+        confirmPanel.SetActive(false);
+    }
+
+    void Start()
+    {
+
+        if (unansweredQuestion == null)
+        {
+            Debug.Log("init");
+            initializeQuestionArray();
+            unansweredQuestion = questionArray.ToList<Question>();
+        }
+        if (unansweredQuestion.Count == 0)
+        {
+            gameOver = true;
+            Debug.Log("gameOver");
+        }
+        if (!gameOver)
+        {
+            setCurrentQuestion();
+            setQuestionText();
+            setAnswersText();
+            listenAnswer();
+            confirmAnswer();
+        }
+
+
+    }
+
+
+
+    void getClickedBtn(int tempAnswer)
+    {
+        temporaryAnswer = tempAnswer;
+    }
+
+
+
+
+    void confirmAnswer()
+    {
+        print("confiremd");
+        confirmBtn.onClick.AddListener(() => checkAnswer(temporaryAnswer));
+
+    }
+
+
+    void initializeQuestionArray()
+    {
+        questionArray[0] = new Question("what is the higest place on earth", "Mount Everest", "Mauna Kea", " Mount Chimborazo", "Kangchenjunga", "Mount Everest");
+        questionArray[1] = new Question("what is the lowest place on mars", "Mount Everest", "Mauna Kea", " Mount Chimborazo", "Kangchenjunga", "Mount Everest");
+        questionArray[2] = new Question("what is the higest place on earth", "Mount Everest", "Mauna Kea", " Mount Chimborazo", "Kangchenjunga", "Mount Everest");
+        questionArray[3] = new Question("what is the higest place on earth", "Mount Everest", "Mauna Kea", " Mount Chimborazo", "Kangchenjunga", "Mount Everest");
+        questionArray[4] = new Question("what is the higest place on earth", "Mount Everest", "Mauna Kea", " Mount Chimborazo", "Kangchenjunga", "Mount Everest");
+        questionArray[5] = new Question("what is the higest place on earth", "Mount Everest", "Mauna Kea", " Mount Chimborazo", "Kangchenjunga", "Mount Everest");
+        questionArray[6] = new Question("what is the higest place on earth", "Mount Everest", "Mauna Kea", " Mount Chimborazo", "Kangchenjunga", "Mount Everest");
+        questionArray[7] = new Question("what is the higest place on earth", "Mount Everest", "Mauna Kea", " Mount Chimborazo", "Kangchenjunga", "Mount Everest");
+        questionArray[8] = new Question("what is the higest place on earth", "Mount Everest", "Mauna Kea", " Mount Chimborazo", "Kangchenjunga", "Mount Everest");
+        questionArray[9] = new Question("what is the higest place on earth", "Mount Everest", "Mauna Kea", " Mount Chimborazo", "Kangchenjunga", "Mount Everest");
+    }
+
+    void listenAnswer()
+    {
+        answer1Btn.onClick.AddListener(() => getClickedBtn(1));
+        answer2Btn.onClick.AddListener(() => getClickedBtn(2));
+        answer3Btn.onClick.AddListener(() => getClickedBtn(3));
+        answer4Btn.onClick.AddListener(() => getClickedBtn(4));
+
+    }
+
+    void setCurrentQuestion()
+    {
+        curentQuestion = unansweredQuestion[0];
+    }
+
+    IEnumerator moveToNextQuestion()
+    {
+        unansweredQuestion.Remove(curentQuestion);
+        yield return new WaitForSeconds(timeForNextQuestion);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    void checkAnswer(int btn)
+    {
+        confirmPanel.SetActive(false);
+        switch (btn)
+        {
+            case 1:
+                if (answer1Text.text == curentQuestion.getCorrectAnswer())
+                {
+                    Debug.Log("Corect");
+                }
+                else
+                {
+                    Debug.Log("incorrect");
+
+                }
+                StartCoroutine(moveToNextQuestion());
+                break;
+            case 2:
+                if (answer2Text.text == curentQuestion.getCorrectAnswer())
+                {
+                    Debug.Log("Corect");
+                }
+                else
+                {
+                    Debug.Log("incorrect");
+
+                }
+                StartCoroutine(moveToNextQuestion());
+
+                break;
+            case 3:
+                if (answer3Text.text == curentQuestion.getCorrectAnswer())
+                {
+                    Debug.Log("Corect");
+
+                }
+                else
+                {
+                    Debug.Log("incorrect");
+                }
+                StartCoroutine(moveToNextQuestion());
+
+                break;
+            case 4:
+                if (answer4Text.text == curentQuestion.getCorrectAnswer())
+                {
+                    Debug.Log("Corect");
+
+                }
+                else
+                {
+                    Debug.Log("incorrect");
+
+                }
+                StartCoroutine(moveToNextQuestion());
+
+                break;
+
+        }
+
+    }
+
+    void setQuestionText()
+    {
+        questionText.text = curentQuestion.getQuestion();
+    }
+    void setAnswersText()
+    {
+        answer1Text.text = curentQuestion.getAnswer1();
+        answer2Text.text = curentQuestion.getAnswer2();
+        answer3Text.text = curentQuestion.getAnswer3();
+        answer4Text.text = curentQuestion.getAnswer4();
+
+
+    }
+
+}
+
---
+++ Millionaire/Assets/Scripts/Question.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Question
+{
+    string question;
+    string answer1;
+    string answer2;
+    string answer3;
+    string answer4;
+    string correctAnswer;
+
+    public Question(string question, string answer1, string answer2, string answer3, string answer4, string correctAnswer)
+    {
+        this.question = question;
+        this.answer1 = answer1;
+        this.answer2 = answer2;
+        this.answer3 = answer3;
+        this.answer4 = answer4;
+        this.correctAnswer = correctAnswer;
+
+    }
+    public string getQuestion() { return question; }
+    public string getAnswer1() { return answer1; }
+    public string getAnswer2() { return answer2; }
+    public string getAnswer3() { return answer3; }
+    public string getAnswer4() { return answer4; }
+    public string getCorrectAnswer() { return correctAnswer; }
+
+}
